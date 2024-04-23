7b8e64a4cb45d96f8277c1c80382d81baff063a3
Giada Sacchi <71716411+giada-sacchi@users.noreply.github.com>
2024-04-23 13:47:09 +0100

    Update streamlit_app.py
    generic syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,10 @@ if ingredients_list: # is not null
         st.subheader(fruit_chosen + ': Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_json = fruityvice_response.json()
-        st.dataframe(data=fv_json['error'],use_container_width=True)
+        try:
+            st.dataframe(data=fv_json['nutritions'],use_container_width=True)
+        except:
+            st.dataframe(data=fv_json,use_container_width=True)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                          values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
