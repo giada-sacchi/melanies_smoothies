4ae4a6d6deb409bd17c38fe237085b905fb61656
Giada Sacchi <71716411+giada-sacchi@users.noreply.github.com>
2024-04-23 13:12:49 +0100

    Update streamlit_app.py
    selecting columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ if ingredients_list: # is not null
         #st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
         st.subheader(fruit_chosen + ': Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True).select(col('nutritions'))
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                          values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
