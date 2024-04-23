104ef4e293cb573bf8df3138fe2dbb67457f338c
Giada Sacchi <71716411+giada-sacchi@users.noreply.github.com>
2024-04-23 13:08:03 +0100

    Update streamlit_app.py
    adding a search column

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,9 @@ st.write('The name on your smoothie will be: ', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -31,16 +32,19 @@ if ingredients_list: # is not null
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
         # display fruityvice nutrition info
+        search_on=pd_df.loc[pd_df['FRUIT_NAME']==fruit_chosen,'SEARCH_ON']-iloc[0]
+        #st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
         st.subheader(fruit_chosen + ': Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-                         values ('""" + ingredients_string + """',
-                         '""" + name_on_order + """')"""
+                         values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         success_message = """Your Smoothie is ordered, """ + name_on_order + """!"""
         st.success(success_message, icon="✅")
+
